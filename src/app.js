e62d3521e9adabb004708c2e00b88d6c66ab7652
Aleco112 <aleco112@yahoo.fr>
2022-03-12 02:02:35 +0000

    new one

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  function generateDomain(top) {
+    let pronoun = ["the", "our"];
+    let adj = ["great", "big"];
+    let noun = ["jogger", "racoon"];
+
+    for (let pindex = 0; pindex < 2; pindex++) {
+      for (aindex = 0; aindex < 2; aindex++) {
+        for (nindex = 0; nindex < 2; nindex++) {
+          console.log(pronoun[pindex] + adj[aindex] + noun[nindex] + top);
+        }
+      }
+    }
+  }
+  generateDomain(".net");
   console.log("Hello Rigo from the console!");
 };
