251668e826f8dbc1ca30e8eacc297254e8b4e926
Aleco112 <aleco112@yahoo.fr>
2022-03-24 00:57:02 +0000

    new-one

--- src/app.js
+++ src/app.js
@@ -13,8 +13,8 @@ window.onload = function() {
     let noun = ["jogger", "racoon"];
 
     for (let pindex = 0; pindex < 2; pindex++) {
-      for (aindex = 0; aindex < 2; aindex++) {
-        for (nindex = 0; nindex < 2; nindex++) {
+      for (let aindex = 0; aindex < 2; aindex++) {
+        for (let nindex = 0; nindex < 2; nindex++) {
           console.log(pronoun[pindex] + adj[aindex] + noun[nindex] + top);
         }
       }
